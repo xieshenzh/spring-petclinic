b0d5016bbc188a4b5e37539fbb0980bbac7894a2
xiezhang <xiezhang@redhat.com>
2022-03-30 22:53:07 -0400

    Workaround for primary key conflict issue with Crunchy when inserting new records The initial value of id is 1, even when there are existing records with id 1

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -15,12 +15,13 @@
  */
 package org.springframework.samples.petclinic.model;
 
-import java.io.Serializable;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
@@ -33,7 +34,8 @@ import javax.persistence.MappedSuperclass;
 public class BaseEntity implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "seq")
+	@GenericGenerator(name = "seq", strategy = "increment")
 	private Integer id;
 
 	public Integer getId() {
